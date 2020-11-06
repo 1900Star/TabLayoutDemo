331e6f93dbf152459834902fad5403bd63518651
Luoshipeng <strangermy98@gmail.com>
2020-11-06 18:12:04 +0800

    【优化】

--- app/src/main/java/com/yibao/tablayoutdemo/MainActivity.java
+++ app/src/main/java/com/yibao/tablayoutdemo/MainActivity.java
@@ -2,22 +2,16 @@ package com.yibao.tablayoutdemo;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
-
-import androidx.annotation.StringRes;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
 import com.yibao.tablayoutdemo.ui.main.SectionsPagerAdapter;
 import com.yibao.tablayoutdemo.view.MainViewPager;
 
--- app/src/main/java/com/yibao/tablayoutdemo/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/yibao/tablayoutdemo/ui/main/PlaceholderFragment.java
@@ -8,12 +8,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,7 +21,6 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.android.material.appbar.AppBarLayout;
 import com.yibao.tablayoutdemo.MyAdapter;
-import com.yibao.tablayoutdemo.MyGridViewAdapter;
 import com.yibao.tablayoutdemo.R;
 import com.yibao.tablayoutdemo.UserInfo;
 import com.yibao.tablayoutdemo.view.ScrollItemView;
